1b2e4974d818ba83e39697fd245be49e5400b9de
arthur andrade <arthurandrad.23@gmail.com>
2023-04-09 21:20:24 -0300

    CSS done

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [temperature, setTemperature] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <div className='container'>
+        <h3>Enter tempeature</h3>
+        <div className='conversionInput'>
+          <p>Degree</p>
+          <input type='number'></input>
+        </div>
+        <div>
+          <button>Convert → </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='results'>
+        <div className='temperaturevalue'>
+          37
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
