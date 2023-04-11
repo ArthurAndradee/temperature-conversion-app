d1f94274f2632e09c7fae990af033cead1885a99
arthur andrade <arthurandrad.23@gmail.com>
2023-04-11 20:20:48 -0300

    finish project

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,16 @@ import './App.css'
 const App = () => {
   const [temperature, setTemperature] = useState(0)
 
-  var value = 1
-
   const handleChange = (event) => {
     setTemperature(event.target.value);
-    value = 2
   };
-
-  const showTemperature = () => {
-    
-  }
 
   return (
     <div className='app'>
       <div className='container'>
         <h3>Enter tempeature</h3>
         <div className='conversionInput'>
-          <p>Degree (celsius)</p>
+          <p>Degree °C</p>
           <input 
           type='number'
           id="temperature"
@@ -29,12 +22,15 @@ const App = () => {
           </input>
         </div>
         <div>
-          <button onClick={showTemperature}>Convert → </button>
+          <button>Automatically converted! </button>
         </div>
       </div>
       <div className='results'>
         <div className='temperaturevalue'>
-          Farenheit: {temperature * 1.8 + 32}
+         {temperature * 1.8 + 32} °F
+        </div>
+        <div className='temperaturevalue'>
+          {temperature - 273.5} °K
         </div>
       </div>
     </div>
